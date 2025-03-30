0508f48b63e11afab7de5f33e1dfc657860c5df4
evidencia <evidenciadavid2016@gmail.com>
2025-03-30 02:56:40 +0100

    feat: add separator between tasks

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,11 @@
 import Button from './Button'
-import { AddIcon, TrashIcon } from '../assets/icons'
+import {
+  AddIcon,
+  CloudSunIcon,
+  MoonIcon,
+  SunIcon,
+  TrashIcon,
+} from '../assets/icons'
 
 function Tasks() {
   return (
@@ -22,6 +28,29 @@ function Tasks() {
             Nova tarefa
             <AddIcon />
           </Button>
+        </div>
+      </div>
+
+      <div className="rounded-xl bg-white p-6">
+        <div className="space-y-3">
+          <div className="flex gap-2 border-b border-solid border-[#F4F4F4] pb-1">
+            <SunIcon />
+            <p className="text-sm text-[#9A9C9F]">Morning</p>
+          </div>
+        </div>
+
+        <div className="my-6 space-y-3">
+          <div className="flex gap-2 border-b border-solid border-[#F4F4F4] pb-1">
+            <CloudSunIcon />
+            <p className="text-sm text-[#9A9C9F]">Afternoon</p>
+          </div>
+        </div>
+
+        <div className="space-y-3">
+          <div className="flex gap-2 border-b border-solid border-[#F4F4F4] pb-1">
+            <MoonIcon />
+            <p className="text-sm text-[#9A9C9F]">Night</p>
+          </div>
         </div>
       </div>
     </div>
