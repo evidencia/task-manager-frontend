9c50d37ad30bcc06645d693bed8ce339a21133da
evidencia <evidenciadavid2016@gmail.com>
2025-04-27 02:04:55 +0100

    feat: ensure task can be deleted

--- src/pages/task-details.jsx
+++ src/pages/task-details.jsx
@@ -96,6 +96,17 @@ const TaskDetailsPage = () => {
     setSaveIsLoading(false)
   }
 
+  const handleDeleteClick = async () => {
+    const response = await fetch(`http://localhost:3000/tasks/${task.id}`, {
+      method: 'DELETE',
+    })
+    if (!response.ok) {
+      return toast.error('Ocorreu um erro ao deletar a tarefa')
+    }
+    toast.success('Tarefa deletada com sucesso')
+    navigate(-1)
+  }
+
   const titleError = errors.find((error) => error.inputName === 'title')
   const timeError = errors.find((error) => error.inputName === 'time')
   const descriptionError = errors.find(
@@ -127,7 +138,11 @@ const TaskDetailsPage = () => {
             <h1 className="mt-2 text-xl font-semibold">{task?.title}</h1>
           </div>
 
-          <Button className="h-fit self-end" color="danger">
+          <Button
+            className="h-fit self-end"
+            color="danger"
+            onClick={handleDeleteClick}
+          >
             {' '}
             <TrashIcon />
             Deletar tarefa
